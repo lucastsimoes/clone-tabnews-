573c971673cc919bb2632e0ce4ef3e3f6d3b4c97
Lucas Simões <lucas@dlatibaia.com.br>
2025-04-01 21:28:50 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const version = await database.query("SHOW server_version;");
+  const versionData = version.rows[0].server_version;
+
+  const maxConnections = await database.query("SHOW max_connections;");
+  const maxConnectionsData = maxConnections.rows[0].max_connections;
+
+  const dbName = process.env.POSTGRES_DB;
+  const openedConnections = await database.query({
+    text: `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [dbName],
+  });
+  const openedConnectionsData = openedConnections.rows[0].count;
   response.status(200).json({
-    chave: "valor",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: versionData,
+        max_connections: parseInt(maxConnectionsData),
+        opened_connections: openedConnectionsData,
+      },
+    },
   });
 }
