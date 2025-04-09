05d612d4ecac376dbfeebc1e21406d8844750a09
Lucas Simões <lucas@dlatibaia.com.br>
2025-04-09 00:25:50 -0300

    add `/api/v1/migrations` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,7 +4,7 @@ export default async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
   const version = await database.query("SHOW server_version;");
-  const versionData = version.rows[0].server_version;
+  const versionData = parseInt(version.rows[0].server_version);
 
   const maxConnections = await database.query("SHOW max_connections;");
   const maxConnectionsData = maxConnections.rows[0].max_connections;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,12 +4,8 @@ test("GET to api/v1/status must return 200", async () => {
   const responseBody = await response.json();
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-  process.env.POSTGRES_DATABASE === "local_db"
-    ? expect(responseBody.dependencies.database.version).toEqual("16.0")
-    : expect(responseBody.dependencies.database.version).toEqual("16.8");
-  process.env.POSTGRES_DATABASE === "local_db"
-    ? expect(responseBody.dependencies.database.max_connections).toEqual(100)
-    : expect(responseBody.dependencies.database.max_connections).toEqual(901);
+  expect(responseBody.dependencies.database.version).toEqual(16);
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
 
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
