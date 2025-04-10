a0835c2cbca0cf169d314e3a425300b3097b1834
Lucas Simões <lucas@dlatibaia.com.br>
2025-04-09 21:48:46 -0300

    teste

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -9,32 +9,38 @@ export default async function migrations(request, response) {
       error: "Method Not Allowed",
     });
   }
-
-  const dbClient = await database.getNewClient();
-  const defaultMigrationsOptions = {
-    dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunnner(defaultMigrationsOptions);
-    await dbClient.end();
-    response.status(200).json(pendingMigrations);
-  } else if (request.method === "POST") {
-    const migratedMigrations = await migrationRunnner({
-      ...defaultMigrationsOptions,
-      dryRun: false,
-    });
-    await dbClient.end();
-    if (migratedMigrations.length > 0) {
-      response.status(201).json(migratedMigrations);
-    } else {
-      response.status(200).json(migratedMigrations);
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const defaultMigrationsOptions = {
+      dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunnner(
+        defaultMigrationsOptions,
+      );
+      response.status(200).json(pendingMigrations);
+    } else if (request.method === "POST") {
+      const migratedMigrations = await migrationRunnner({
+        ...defaultMigrationsOptions,
+        dryRun: false,
+      });
+      if (migratedMigrations.length > 0) {
+        response.status(201).json(migratedMigrations);
+      } else {
+        response.status(200).json(migratedMigrations);
+      }
     }
+    await dbClient.end();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient.end();
   }
-  await dbClient.end();
-  return response.status(405);
 }
