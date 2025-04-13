3543981ff28017931150b50fcae806ee602e1b02
Lucas Simões <lucas@dlatibaia.com.br>
2025-04-13 03:11:25 -0300

    teste

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -10,7 +10,7 @@ test("GET to api/v1/status must return 200", async () => {
   const responseBody = await response.json();
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-  expect(responseBody.dependencies.database.version).toEqual(21);
+  expect(responseBody.dependencies.database.version).toEqual(16);
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
 
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
