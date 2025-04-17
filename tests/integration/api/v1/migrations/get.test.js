af44f3891836b72d351c9d21166590c5b3da03b0
Lucas Simões <lucas@dlatibaia.com.br>
2025-04-17 00:03:30 -0300

    refactor: use `orchestrator.clearDatabase()` in tests

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,9 +1,8 @@
-import database from "infra/database";
 import orchestrator from "tests/orchestrator";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await database.query("DROP schema public cascade; CREATE schema public;");
+  await orchestrator.clearDatabase();
 });
 
 describe("GET /api/v1/migrations ", () => {
@@ -11,7 +10,6 @@ describe("GET /api/v1/migrations ", () => {
     test("Retrieving pending migrations", async () => {
       const response = await fetch("http://localhost:3000/api/v1/migrations");
       expect(response.status).toBe(200);
-
       const responseBody = await response.json();
       expect(Array.isArray(responseBody)).toBe(true);
       expect(responseBody.length).toBeGreaterThan(0);
