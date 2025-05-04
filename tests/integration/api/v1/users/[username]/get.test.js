8924e24aa38ce15af3c6886304d1ccb35d21d398
Lucas Simões <lucas@dlatibaia.com.br>
2025-05-04 17:46:19 -0300

    feat: hash `password` in `user.create`

--- tests/integration/api/v1/users/[username]/get.test.js
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -33,7 +33,7 @@ describe("GET /api/v1/users/[username] ", () => {
         id: response2Body.id,
         username: "MesmoCase",
         email: "mesmo.case@gmail.com",
-        password: "abc123",
+        password: response2Body.password,
         created_at: response2Body.created_at,
         updated_at: response2Body.updated_at,
       });
@@ -65,7 +65,7 @@ describe("GET /api/v1/users/[username] ", () => {
         id: response2Body.id,
         username: "caseDiferente",
         email: "case.diferente@gmail.com",
-        password: "abc123",
+        password: response2Body.password,
         created_at: response2Body.created_at,
         updated_at: response2Body.updated_at,
       });
